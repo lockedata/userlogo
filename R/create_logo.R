b0d5f0b19c5d0333e023e4bb4b277b9ef6f04a83
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-11-14 12:54:52 +0100

    fix

--- R/create_logo.R
+++ R/create_logo.R
@@ -25,11 +25,11 @@ create_logo <- function(colour = "#ffc0cb",
   logo <- xml2::read_xml(logo_path)
 
   col_path <- xml2::xml_find_all(logo,
-                                 "//d1:path",
-                                 xml2::xml_ns(logo))[3]
+                                 "//d1:path[@id='sides']",
+                                 xml2::xml_ns(logo))
 
   current_style <- xml2::xml_attr(col_path, "style")
-  new_style <- sub("#494949", colour,
+  new_style <- sub("#2165b6", colour,
                    current_style)
   xml2::xml_set_attr(col_path, "style", new_style)
 
@@ -43,7 +43,8 @@ create_logo <- function(colour = "#ffc0cb",
 
 
   # year
-  year_parts <- xml2::xml_find_all(logo, "//d1:tspan", xml2::xml_ns(logo))
+  year_parts <- xml2::xml_find_all(logo, "//d1:text[@id='y1' or @id='y2' or @id='y3' or @id='y4']",
+                                   xml2::xml_ns(logo))
   xml2::xml_set_text(year_parts, substring(year, 1:4, 1:4))
 
   # save
